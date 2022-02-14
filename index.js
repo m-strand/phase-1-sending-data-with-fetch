2d656a0b82e17fc1c559aaa9edf8dab3b13b8e45
Brooke Strand <brooke.strand007@gmail.com>
2022-02-14 10:01:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: username,
+      email: useremail,
+    }),
+  };
+
+  function submitData(username, useremail) {
+        return fetch('http://localhost:3000/users', configurationObject)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+            document.append(object);
+        })
+        .catch(function (error) {
+            console.log(error.message);
+            document.append(error.message)
+          });
+    }
